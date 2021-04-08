7082d158a2dae81c17409599ce9b731117b8a615
vitala1506 <65966843+vitala1506@users.noreply.github.com>
2021-04-08 20:44:50 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,37 @@
+//alert("Hello, World");
+
+var one = "Hello";
+
+//alert(one); 
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+//console.log(arr[2]);
+
+//alert("hello, world");
+//let answer = confirm("are you here?");
+//console.log(answer);
+
+/* let answer = +prompt("are you here?", "yes");
+console.log(typeof(answer));
+
+console.log("arr" + "- object") */
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+dect--;
+
+console.log(incr);
