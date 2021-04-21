05cb7fcd06dcb9b49d3c87fda962b703404bed29
vitala1506 <65966843+vitala1506@users.noreply.github.com>
2021-04-21 20:42:09 +0300

    lesson 3

--- js.js
+++ js.js
@@ -47,72 +47,71 @@ let appData = {
     optionalExpenses: {},
     income: [],
     timeData: time,
-    savings: true
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("The most important cost", ''),
+                b = prompt("How much?", '');
+
+            if ((typeof(a)) === 'string' && (typeof(a) != null && (typeof(b)) != null) &&
+                a != '' && b != '' && a.length < 50) {
+                console.log("done");
+                appData.expenses[a] = b;
+            } else {
+                i = i - 1;
+            }
+        }
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert(appData.budget / 30);
+        alert("budget for month " + appData.moneyPerDay + "rub.");
+    },
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log("the minimum is!");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log("it is middle!");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("it is hight!");
+        } else {
+            console.log("errow");
+        }
+    },
+    checkSaving: function() {
+        if (appData.savings == true) {
+            let save = +prompt("what is the summma of saving?"),
+                percent = +prompt("How is procent?");
+
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Income for depoxit:" + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function() {
+        for (let i = 1; i <= 3; i++) {
+            let questionOptExpenses = prompt("Статья необязательных расходов?");
+            appData.optionalExpenses[i] = questionOptExpenses;
+            console.log(appData.optionalExpenses);
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt("what is income? (input with point)", "");
+
+        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
+            console.log("you input the wrong data");
+        } else {
+            appData.income = items.split(", ");
+            appData.income.push(prompt("something else?"));
+            appData.income.sort();
+        }
+
+        appData.income.forEach(function(itemmassive, i) {
+            alert("mothods of income: " + (i + 1) + " - " + itemmassive);
+        });
+    }
+
 };
 
-
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("The most important cost", ''),
-            b = prompt("How much?", '');
-
-        if ((typeof(a)) === 'string' && (typeof(a) != null && (typeof(b)) != null) &&
-            a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expenses[a] = b;
-        } else {
-            i = i - 1;
-        }
-    }
-}
-chooseExpenses();
-
-
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert(appData.budget / 30);
-    alert("budget for month " + appData.moneyPerDay + "rub.");
-}
-detectDayBudget();
-
-
-function detectLevel() {
-    if (appData.moneyPerDay < 100) {
-        console.log("the minimum is!");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log("it is middle!");
-    } else if (appData.moneyPerDay > 2000) {
-        console.log("it is hight!");
-    } else {
-        console.log("errow");
-    }
-}
-detectLevel();
-
-
-function checkSaving() {
-    if (appData.savings == true) {
-        let save = +prompt("what is the summma of saving?"),
-            percent = +prompt("How is procent?");
-
-        appData.monthIncome = save / 100 / 12 * percent;
-        alert("Income for depoxit:" + appData.monthIncome);
-    }
-}
-checkSaving();
-
-
-function chooseOptExpenses() {
-    for (let i = 1; i < 4; i++) {
-        let rest = +prompt("what is rest?");
-
-        if ((typeof(a)) === 'string' && (typeof(a) != null && (typeof(b)) != null) &&
-            a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expenses[i] = rest;
-        } else {
-            i = i - 1;
-        }
-
-    }
+for (let key in appData) {
+    console.log("our program include: " + key + " - " + appData[key]);
 }
--- probnik.js
+++ probnik.js
@@ -1,5 +1,49 @@
-function calc(a, b) {
-    return (a + b);
+/* let options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
+
+console.log(options.name);
+
+options.bool = false;
+options.colors = {
+    border: "black",
+    bg: "red"
+};
+
+delete options.bool;
+
+console.log(options);
+
+for (let key in options) {
+    console.log('property' + key + ' have' + options[key]);
 }
 
-console.log(calc(23, 43));
+console.log(Object.keys(options).length); */
+
+/*  
+
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+} */
+
+/* arr.forEach(function(item, i, mass) {
+console.log(i + ': ' + item + "(arr:" + mass + ')');
+});
+console.log(arr); */
+
+let ans = prompt("", "");
+arr = [];
+
+arr = ans.split(',');
+console.log(arr);
+
+let arr = ["asdf", 'qwdf', 'sdf', '1', '2563', '223'],
+    i = arr.sort(compareNum);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+console.log(arr);
